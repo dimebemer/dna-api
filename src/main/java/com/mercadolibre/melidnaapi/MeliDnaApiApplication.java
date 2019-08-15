25b0d66ffd8a4dcc3f2e916e3f9e21cb80a29cf3
Rafael Bemerguy <rafabemerguy@gmail.com>
2019-08-15 17:03:45 -0300

    starting project

---
+++ src/main/java/com/mercadolibre/melidnaapi/MeliDnaApiApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.melidnaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliDnaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliDnaApiApplication.class, args);
+	}
+
+}
